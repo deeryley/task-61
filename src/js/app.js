71707b19b1f00a61e9a21cda2c84d81732b3bf97
Vasilena <vt.merica@gmail.com>
2022-02-15 15:37:30 +0200

    add price class

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-var hotemoji = document.querySelectorAll(".hot");
-hotemoji.innerHTML = hotemoji.innerHTML =+ "🔥"
+var hotemoji = document.querySelectorAll("price.hot");
+hotemoji.innerHTML = hotemoji.innerHTML +"🔥"
+
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
