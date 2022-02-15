2b3e0095bc97ccd0ac7a6df90369716e323a63ee
Vasilena <vt.merica@gmail.com>
2022-02-15 15:02:15 +0200

    add fire

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
-import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const hot = document.querySelectorAll(".hot");
+  hot.innerHTML += "  🔥"
+  
 });
